89c4ef5c95016b2ff0ee67acd1cbd7663d5a8ce5
Granithashani1 <125699596+Granithashani1@users.noreply.github.com>
2023-03-14 00:11:42 +0100

    Space

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,6 @@ import {} from "../img/About.css";
 const About = () => {
   return (
     <>
-    <iframe width="100%" height="315" src="https://www.youtube.com/embed/v9sns8JXJ2M?autoplay=1&mute=1&loop=1&playlis=2XhPa-1pl4Y&controls=0"></iframe>
       <Container className="methodcards">
         <Row>
           {data.map((item) => (
